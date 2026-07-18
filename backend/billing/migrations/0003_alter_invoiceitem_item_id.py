4194312d96e14cd9d99c267062cf1f9ab6f802bc
Christin johny <christinjohnymankuthel.com>
2026-07-18 16:35:57 +0530

    Fix migration for item_id postgres cast issue

--- backend/billing/migrations/0003_alter_invoiceitem_item_id.py
+++ backend/billing/migrations/0003_alter_invoiceitem_item_id.py
@@ -10,7 +10,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.RemoveField(
+            model_name='invoiceitem',
+            name='item_id',
+        ),
+        migrations.AddField(
             model_name='invoiceitem',
             name='item_id',
             field=models.BigIntegerField(blank=True, null=True),
